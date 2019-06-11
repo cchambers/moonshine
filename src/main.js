d4647710941dd8aa31b8cbbe847a587170c3f707
Chris Chambers <chris_chambers@belk.com>
2019-06-11 11:38:35 -0400

    progress

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import Vue from 'vue'
 Vue.config.productionTip = false
 
 // base styles...
+require('./assets/style/_variables.scss')
+require('./assets/style/_mixins.scss')
 require('./assets/style/base.scss')
-require('./assets/style/material.scss')
+require('./assets/style/layout.scss')
 
 // components
 require('./components/sh-button')
