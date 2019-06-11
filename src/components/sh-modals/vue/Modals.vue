54291983b6b27ee1ea882434a2c830dbeaf724e1
Chris Chambers <chris_chambers@belk.com>
2019-06-11 16:08:34 -0400

    close

--- src/components/sh-modals/vue/Modals.vue
+++ src/components/sh-modals/vue/Modals.vue
@@ -6,3 +6,8 @@
   
 <script src="../script/Modals.js"></script>
 <style lang="scss" src="../style/default.scss" scoped></style>
+<style lang="scss">
+  [modal-trigger] {
+    color: #3c7cff;
+  }
+</style>
