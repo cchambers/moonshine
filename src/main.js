74ff2e4f058d1a0b0125cabb586f4d375dd0f891
Chris Chambers <chris_chambers@belk.com>
2019-06-11 12:03:25 -0400

    more progress

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
-
 Vue.config.productionTip = false
 
 // base styles...
-require('./assets/style/_variables.scss')
-require('./assets/style/_mixins.scss')
 require('./assets/style/base.scss')
 require('./assets/style/layout.scss')
 
@@ -12,4 +9,4 @@ require('./assets/style/layout.scss')
 require('./components/sh-button')
 
 // mount to container
-new Vue().$mount('#shine')
+new Vue().$mount('#xxx')
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    runtimeCompiler: true,
+  runtimeCompiler: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `
+          @import "@/assets/style/_variables.scss";
+          @import "@/assets/style/_mixins.scss";
+        `
+      }
+    }
+  }
 }
