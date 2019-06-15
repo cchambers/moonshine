2d70716af9e015e2ce3a623a9c3b025a2c04690c
Chris Chambers <chris_chambers@belk.com>
2019-06-15 11:04:01 -0400

    nav design updates, search, etc

--- src/main.js
+++ src/main.js
@@ -3,10 +3,7 @@ import Vue from 'vue'
 
 Vue.config.productionTip = false
 
-require('./assets/script/core.js')
-
-// base styles...
-require('./assets/style/base.scss')
+  // base styles...
 require('./assets/style/layout.scss')
 
 // components
@@ -17,11 +14,12 @@ require('./components/sh-header')
 require('./components/sh-modals')
 require('./components/sh-nav')
 
-// if (process.env.VUE_APP_DEV) {
-//   require('./components/lib-app')
-//   require('./components/lib-toolbar')
-//   require('./components/component-template')
-// } 
+if (process.env.VUE_APP_DEV) {
+  require('./assets/script/core.js')
+  require('./assets/style/base.scss')
+  require('./components/lib-toolbar')
+  require('./components/component-template')
+} 
 
 // mount to container
 new Vue().$mount('#xxx')
