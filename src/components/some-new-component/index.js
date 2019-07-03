a0fcb9617707d5bf35d1dc8012cea77e7c6facc4
Chris Chambers <chris_chambers@belk.com>
2019-07-03 14:57:59 -0400

    remove some broken thing

--- src/components/some-new-component/index.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import SomeNewComponent from './vue/SomeNewComponent';
-
-Vue.customElement('some-new-component', SomeNewComponent);
--- src/components/some-new-component/vue/SomeNewComponent.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="some-new-component" v-bind:class="{ 'snap-state': snapping }" v-on:click="snap">
-    <slot>{{ msg }}</slot>
-  </div>
-</template>
-
-<script src="../script/SomeNewComponent.js"></script>
-<style lang="scss" src="../style/default.scss" scoped></style>
-<style lang="scss" src="../style/primary.scss" scoped></style>
