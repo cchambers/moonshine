3f9fbb7643d9c3bc7b39d3e20c4d97e6c8832828
Chris Chambers <chris_chambers@belk.com>
2019-07-04 15:59:49 -0400

    allow for any header in accordion

--- src/components/sh-accordion/vue/Accordion.vue
+++ src/components/sh-accordion/vue/Accordion.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="sh-accordion" v-bind:class="{ 'active': ariaExpanded }" v-on:click="toggleActive">
-    <h3 class="header" :id="headerId" :aria-controls="ariaControlsId">
+    <div class="header" :id="headerId" :aria-controls="ariaControlsId">
       <slot name="header"></slot>
-    </h3>
+    </div>
     <div :aria-expanded="ariaExpanded" :aria-labelled-by="headerId" role="region">
       <div :id="ariaControlsId" class="collapse">
         <div class="body">
@@ -14,5 +14,5 @@
 </template>
 
 <script src="../script/Accordion.js"></script>
-<style lang="scss" src="../style/default.scss" scoped></style>
-<style lang="scss" src="../style/primary.scss" scoped></style>
+<style lang="scss" src="../style/default.scss"></style>
+<style lang="scss" src="../style/primary.scss"></style>
