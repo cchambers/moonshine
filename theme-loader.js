c78e0015fe3a374c9a84c1b6a85f28cfa3ba9e1e
Chris Chambers <chris_chambers@belk.com>
2019-07-06 21:36:10 -0400

    theme loader semi-functional

--- theme-loader.js
+++ theme-loader.js
@@ -1,4 +1,5 @@
 "use strict";
+const fs = require('fs-extra');
 
 /**
  * The sass-loader makes node-sass available to webpack modules.
@@ -8,32 +9,20 @@
  */
 function themeLoader(content) {
 
-  content = `${content}
-    [theme="dark"] {
-      $lightpurple: #BB86FC;
-      $purple: #3700B3;
-      $green: #03DAC6;
-    
-      $highlight-primary: rgb(157, 208, 230);
-      $highlight-secondary: rgb(25,25,25);
-      $highlight-tertiary: #03DAC6;
-      $highlight-quaternary: #4e1b96;
-    
-      $lowlight-primary: $purple;
-      $lowlight-secondary: #272822;
-      $lowlight-tertiary: rgb(200,200,200);
-      $lowlight-quaternary: #222;
-      $lowlight-quinary: $purple;
-    
-      $accent-primary: #BB86FC;
-      $accent-secondary:#03DAC6;
-      $accent-tertiary: #BB86FC;
-      $accent-quaternary: $purple;
-      $accent-quinary: $purple;
-    
-      $back-primary: $lowlight-secondary;
-      ${content}
-    }`;
+  let themes = ['dark'];
+  let themeData = [content];
+
+  for (let x = 0, l = themes.length; x < l; x++) {
+    let theme = themes[x];
+    let vars = fs.readFileSync(`./src/assets/style/themes/${theme}/_variables.scss`, 'utf8');
+    let split = vars.split("}");
+    split.push(content);
+    split.push('}');
+    let data = split.join('');
+    themeData.push(data);
+  }
+
+  content = themeData.join('');
   return content;
 }
 
