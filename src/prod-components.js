d398e0130a116ac750907a3c20f0809ecef73766
Chris Chambers <chris_chambers@belk.com>
2019-08-05 16:04:50 -0400

    merge down

--- src/prod-components.js
+++ src/prod-components.js
@@ -8,4 +8,5 @@ require('./components/sh-curtain');
 require('./components/sh-modal');
 require('./components/sh-nav-item');
 require('./components/sh-banner');
+require('./components/sh-button');
 require('./components/sh-adapt-content');
