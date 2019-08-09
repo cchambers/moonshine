76dc4eaa9668a1f2f54a8456669d9862839f69dd
Chris Chambers <chris_chambers@belk.com>
2019-08-09 13:03:42 -0400

    takeover

--- src/prod-components.js
+++ src/prod-components.js
@@ -1,13 +1,13 @@
-require('./components/belk-bag');
-require('./components/belk-coupon');
-require('./components/belk-header');
-require('./components/belk-logo');
-require('./components/belk-search');
+// require('./components/belk-bag');
+// require('./components/belk-coupon');
+// require('./components/belk-header');
+// require('./components/belk-logo');
+// require('./components/belk-search');
 require('./components/belk-takeover');
 
-require('./components/sh-curtain');
+// require('./components/sh-curtain');
 require('./components/sh-modal');
-require('./components/sh-nav-item');
-require('./components/sh-banner');
-require('./components/sh-button');
-require('./components/sh-adapt-content');
+// require('./components/sh-nav-item');
+// require('./components/sh-banner');
+// require('./components/sh-button');
+// require('./components/sh-adapt-content');
