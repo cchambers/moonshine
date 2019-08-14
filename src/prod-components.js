c1bbac2b37b4af09f4be4600f9505ea853bb9f55
Chris Chambers <chris_chambers@belk.com>
2019-08-14 09:29:04 -0400

    revert prod component file

--- src/prod-components.js
+++ src/prod-components.js
@@ -1,8 +1,9 @@
 
-// require('./components/belk-bag');
-// require('./components/belk-header');
-// require('./components/belk-logo');
-// require('./components/belk-search');
+
+require('./components/belk-bag');
+require('./components/belk-header');
+require('./components/belk-logo');
+require('./components/belk-search');
 require('./components/belk-takeover');
 
 require('./components/sh-curtain');
