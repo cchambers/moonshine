7b3370be3582e9805bd84dd4bbaaeff0545620d4
Chris Chambers <chris_chambers@belk.com>
2019-08-28 11:59:42 -0400

    order of files

--- src/prod-components.js
+++ src/prod-components.js
@@ -1,11 +1,12 @@
 
+require('./components/sh-curtain');
+require('./components/sh-modal');
+require('./components/sh-nav-item');
+require('./components/sh-svglib');
+
 require('./components/belk-icon');
 require('./components/belk-takeover');
 require('./components/belk-search');
 require('./components/belk-products');
 require('./components/belk-logo');
 
-require('./components/sh-curtain');
-require('./components/sh-modal');
-require('./components/sh-nav-item');
-require('./components/sh-svglib');
