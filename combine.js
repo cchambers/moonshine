8e7b4a4e8dd4794bd3abefafca358314332f7a86
Chris Chambers <chris_chambers@belk.com>
2019-09-06 15:48:49 -0400

    more tooling, auto deploy to repo

--- combine.js
+++ combine.js
@@ -1,4 +1,21 @@
 const concat = require('concat');
-concat(['./dist/js/chunk-vendors.js', './dist/js/index.js'], './dist/js/shine.js')
+const fs = require('fs');
+var path = require('path');
+const dotenv = require('dotenv');
+let envConfig = dotenv.parse(fs.readFileSync('.env'))
+let promise = concat(['./dist/js/chunk-vendors.js', './dist/js/index.js'], './dist/js/shine.js');
 
-// env.BELK_REPO
+function moveFile(dir, dest) {
+    fs.copyFile(dir, dest, (err) => {
+        if (err) throw err;
+        console.log('Shine.js copied to main repo.');
+    });
+}
+
+promise.then(function() {
+    if (envConfig.SHINE_SCRIPT_PATH) {
+        let dest = envConfig.SHINE_SCRIPT_PATH;
+        let dir = path.join(__dirname, '/dist/js/shine.js');
+        moveFile(dir, dest);
+    }
+});
