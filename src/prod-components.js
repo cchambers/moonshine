534d12d5af326cabf1d74cfc45e69fe58d2de10f
Chris Chambers <chris_chambers@belk.com>
2019-11-05 13:29:43 -0500

    rework icon JSON loading

--- src/prod-components.js
+++ src/prod-components.js
@@ -10,7 +10,6 @@ require('./components/belk-header');
 require('./components/belk-icon');
 require('./components/belk-logo');
 require('./components/belk-products');
-require('./components/belk-product-detail');
 require('./components/belk-promo');
 require('./components/belk-search');
 require('./components/belk-swatch');
