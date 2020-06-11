7f55d04d69b4236bdb08ff7b88f1b55a22a9214e
Chris Chambers <chris_chambers@belk.com>
2020-06-11 14:20:29 -0400

    Lots of stuff, grid, flex, contact page changes

---
+++ src/components/belk-flex/vue/BelkFlex.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="belk-flex"
+    :variant="variant">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import ComponentPrototype from '../../component-prototype';
+
+export default {
+  mixins: [ComponentPrototype],
+
+  name: 'BelkFlex',
+
+  props: {
+    variant: {
+      type: String,
+      default: 'default',
+    },
+  },
+};
+</script>
+<style lang="scss" src="../style/default.scss"></style>
+<style lang="scss" src="../style/primary.scss"></style>
---
+++ src/components/belk-flex/vue/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+import Vue from 'vue';
+import BelkFlex from './BelkFlex.vue';
+
+Vue.customElement('belk-flex', BelkFlex);
+
+
+/*
+* Unit tests:
+* https: //vue-test-utils.vuejs.org/guides/getting-started.html
+*/
+// describe('BelkFlex', () => {
+//   // Inspect the raw component options
+//   it('has a `snap` method', () => {
+//     expect(typeof BelkFlex.methods.snap).toBe('function');
+//   });
+// });
