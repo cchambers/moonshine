d2e2266a26c5bf1520a6515682e0094b3ad72e89
Chris Chambers <chris_chambers@belk.com>
2020-08-17 15:53:05 -0400

    remove polyfill

--- src/assets/script/polyfills.js
+++ src/assets/script/polyfills.js
@@ -115,4 +115,23 @@
       });
     });
   })([Element.prototype, Document.prototype, DocumentFragment.prototype]);
+
+  (function (arr) {
+    arr.forEach(function (item) {
+      if (item.hasOwnProperty('remove')) {
+        return;
+      }
+      Object.defineProperty(item, 'remove', {
+        configurable: true,
+        enumerable: true,
+        writable: true,
+        value: function remove() {
+          if (this.parentNode === null) {
+            return;
+          }
+          this.parentNode.removeChild(this);
+        }
+      });
+    });
+  })([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
 }());
