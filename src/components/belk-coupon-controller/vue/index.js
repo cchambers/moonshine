b0f8e88f39ce313cd88e8a6787fca23609be4d08
Chris Chambers <chris_chambers@belk.com>
2020-08-21 13:24:48 -0400

    more progress

--- src/components/belk-coupon-controller/vue/index.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable no-undef */
-import Vue from 'vue';
-import BelkCouponController from './BelkCouponController.vue';
-
-Vue.customElement('belk-coupon-controller', BelkCouponController);
