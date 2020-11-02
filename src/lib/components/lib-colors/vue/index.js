f105f4356891da6de3f0eaa68533c92f16cd6924
Chris Chambers <chris_chambers@belk.com>
2020-11-02 14:13:25 -0500

    Color libray

---
+++ src/lib/components/lib-colors/vue/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import Vue from 'vue';
+import LibColors from './LibColors.vue';
+
+Vue.customElement('lib-colors', LibColors);
+
+/*
+* Unit tests:
+* https: //vue-test-utils.vuejs.org/guides/getting-started.html
+*/
+// describe('LibColors', () => {
+//   // Inspect the raw component options
+//   it('has a `snap` method', () => {
+//     expect(typeof LibColors.methods.snap).toBe('function');
+//   });
+// });
