5d65b2c76cabcda83fb32ae30a4e1a921b02e10a
Miguel Fondeur <miguel_fondeur@belk.com>
2020-12-08 11:12:30 -0500

    add meganav

---
+++ src/components/belk-meganav/vue/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import Vue from 'vue';
+import BelkMeganav from './BelkMeganav.vue';
+
+Vue.customElement('belk-meganav', BelkMeganav);
+
+/*
+* Unit tests:
+* https: //vue-test-utils.vuejs.org/guides/getting-started.html
+*/
+// describe('BelkMeganav', () => {
+//   // Inspect the raw component options
+//   it('has a `snap` method', () => {
+//     expect(typeof BelkMeganav.methods.snap).toBe('function');
+//   });
+// });
