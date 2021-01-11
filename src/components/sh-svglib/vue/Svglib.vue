cb7d7cbfd065c2457cc29056575aec9e4f322cd5
Chris Chambers <chris_chambers@belk.com>
2021-01-11 10:55:09 -0500

    Icons: fix load

--- src/components/sh-svglib/vue/Svglib.vue
+++ src/components/sh-svglib/vue/Svglib.vue
@@ -123,7 +123,6 @@ export default {
   methods: {
     setup(el) {
       const target = el;
-      console.log('SETUP', el);
       this.$set(this.activeIcons, target.name, this.icons[target.name]);
       target.viewbox = this.icons[target.name].viewbox;
       target.configured = true;
