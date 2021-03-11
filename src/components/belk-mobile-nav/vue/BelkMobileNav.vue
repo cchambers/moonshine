eaf5ca68dd878973a613e960be30bfffbe1b27a9
Chris Chambers <chris_chambers@belk.com>
2021-03-11 12:37:16 -0500

    ADA controls

--- src/components/belk-mobile-nav/vue/BelkMobileNav.vue
+++ src/components/belk-mobile-nav/vue/BelkMobileNav.vue
@@ -10,8 +10,9 @@
     <div class="overlay" @click="close"></div>
     <div class="container">
       <div class="tab-lock" v-if="active" v-on:focus="focusButton()" tabindex="0"></div>
-      <button v-hammer:tap="close"
+      <button @click="close"
         type="button"
+        ref="toggle"
         class="top flex back-highlight-secondary lowlight-primary bold pad-x-micro px-18 lh-50">
         Menu
         <belk-icon name="close" height="22" width="22"
@@ -67,13 +68,21 @@ export default {
   methods: {
     events() {
       this.$bus.$on('show-mobile-nav', this.open);
+      this.$bus.$on('close-modals', this.close);
     },
     open() {
-      if (!this.active) this.active = true;
+      if (!this.active) {
+        document.documentElement.classList.add('scroll-block');
+        this.active = true;
+        this.$bus.$emit('modal-opened', this.uniqueId);
+        setTimeout(() => {
+          this.$refs.toggle.focus();
+        }, 200);
+      }
     },
     close() {
       if (this.active) {
-        document.documentElement.classList.remove('sh-modal-open');
+        document.documentElement.classList.remove('scroll-block');
         this.active = false;
         this.$bus.$emit('modal-closed', this.uniqueId);
       }
