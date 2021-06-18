80e523fbb8edf2fb0b25d0a4687ad5e8e99b89f8
Chris Chambers <chris_chambers@belk.com>
2021-06-18 15:38:45 -0400

    Facet Nav: initial

--- src/components/belk-facet-nav/vue/BelkFacetNav.vue
+++ src/components/belk-facet-nav/vue/BelkFacetNav.vue
@@ -1,9 +1,77 @@
 <template>
   <div class="belk-facet-nav"
-    :variant="variant"
-    v-bind:class="{ 'snap-state': snapping }"
-    v-hammer:tap="snap">
-    <slot name="test">{{ msg }}</slot>
+    :variant="variant">
+    <div ref="links" class="facet-links">
+      <h3>
+        Title
+      </h3>
+      <ul>
+        <li>
+          <a href="#" data-qty="20">link</a>
+        </li>
+        <li>
+          <a href="#" data-qty="20">link</a>
+        </li>
+        <li>
+          <a href="#" data-qty="20">link</a>
+        </li>
+        <li>
+          <a href="#" data-qty="20">link</a>
+        </li>
+      </ul>
+    </div>
+    <div ref="shops" class="facet-shops"></div>
+    <div ref="brands" class="facet-brands">
+      <h3>Brand</h3>
+      <ul>
+        <li>
+          <a href="#" data-qty="20">link</a>
+        </li>
+        <li>
+          <a href="#" data-qty="20">link</a>
+        </li>
+        <li>
+          <a href="#" data-qty="20">link</a>
+        </li>
+        <li>
+          <a href="#" data-qty="20">link</a>
+        </li>
+      </ul>
+    </div>
+    <div ref="colors" class="facet-colors">
+      <sh-accordion>
+        <h3 slot="header">
+          Colors
+        </h3>
+        <ul slot="body">
+          <li>
+            <button>
+              <div class="swatch" style="background: red"></div>
+              <div class="name">Red</div>
+            </button>
+          </li>
+          <li>
+            <button class="active">
+              <div class="swatch" style="background: green"></div>
+              <div class="name">Green</div>
+            </button>
+          </li>
+          <li>
+            <button>
+              <div class="swatch" style="background: blue"></div>
+              <div class="name">Blue</div>
+            </button>
+          </li>
+          <li>
+            <button></button>
+          </li>
+        </ul>
+      </sh-accordion>
+    </div>
+    <div ref="sizes" class="facet-sizes"></div>
+    <div ref="prices" class="facet-prices"></div>
+    <div ref="coupons" class="facet-coupons"></div>
+    <div ref="promos" class="facet-promos"></div>
   </div>
 </template>
 
