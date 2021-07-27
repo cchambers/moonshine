50f4fb302dbc42ba04b05abbf4a555586a5dc888
Chris Chambers <chris_chambers@belk.com>
2021-07-27 15:38:13 -0400

    Facet: keycode fun

--- src/components/belk-facet-nav/vue/BelkFacetNav.vue
+++ src/components/belk-facet-nav/vue/BelkFacetNav.vue
@@ -680,9 +680,9 @@ export default {
 
     sanitizePrice(event){
       const key = event.keyCode;
+      console.log(key);
       const good = (key >= 48 && key <= 57)
-        || (key >= 96 && key <= 105)
-        || (key === 190 || key === 110|| key === 46);
+        || (key === 190 || key === 46);
       if (!good) event.preventDefault();
     },
 
